0c1328a300682531a15c706dcea3313c2c7e4f8f
David28p <157123394+David28p@users.noreply.github.com>
2024-12-18 19:20:37 +0000

    David28p-React

---
+++ src/js/components/card.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Card = () => {
+       return (
+        <div className="row row-cols-1 row-cols-md-4 g-4 text-center">
+        <div className="col">
+          <div className="card h-100 w-55">
+            <img src="https://tse1.mm.bing.net/th?id=OIP.6E8ylMoN9AfN7qaff2OF_wHaE0&pid=Api&P=0&h=180" className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div className="card-footer">
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100 w-55">
+            <img src="https://tse1.mm.bing.net/th?id=OIP.6E8ylMoN9AfN7qaff2OF_wHaE0&pid=Api&P=0&h=180" className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            <div className="card-footer">
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100 w-55">
+            <img src="https://tse1.mm.bing.net/th?id=OIP.6E8ylMoN9AfN7qaff2OF_wHaE0&pid=Api&P=0&h=180" className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+            <div className="card-footer">
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100 w-55">
+            <img src="https://tse1.mm.bing.net/th?id=OIP.6E8ylMoN9AfN7qaff2OF_wHaE0&pid=Api&P=0&h=180" className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+            <div className="card-footer">
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+   
+     
+      )
+}
+ export default Card;
